756b8aba6dab05ad102e36962f2cfbf809b5f42a
Stefan Kruijt <stefankruijt1991@gmail.com>
2015-06-07 00:43:49 +0200

    Changed keepalive to true.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
             hostname: '127.0.0.1',
             port: 9000,
             base: '.', // Project root 
-            keepalive: false,
+            keepalive: true,
             open: false
             }
          }
