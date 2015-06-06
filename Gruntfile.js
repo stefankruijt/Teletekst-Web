a62b3f0eac81c287a7823a94c15607af0dfd0b7a
Stefan Kruijt <stefankruijt1991@gmail.com>
2015-06-06 23:47:02 +0200

    Gruntfile improved and formated

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,52 +1,46 @@
 module.exports = function(grunt) {
-grunt.initConfig({
-    php: {
-        dist: {
-            options: {
-                hostname: '127.0.0.1',
-                port: 9000,
-                base: '.', // Project root 
-                keepalive: false,
-                open: false
+   grunt.initConfig({
+      php: {
+         dist: {
+         options: {
+            hostname: '127.0.0.1',
+            port: 9000,
+            base: '.', // Project root 
+            keepalive: false,
+            open: false
             }
-        }
-    },
-    browserSync: {
-        dist: {
+         }
+      },
+      browserSync: {
+         dist: {
             bsFiles: {
-                src: [
-                    '*'
-                ]
+               src: [
+                  '*'
+               ]
             },
             options: {
-                proxy: '<%= php.dist.options.hostname %>:<%= php.dist.options.port %>',
-                watchTask: true,
-                notify: true,
-                open: true,
-                logLevel: 'silent',
-                ghostMode: {
-                    clicks: true,
-                    scroll: true,
-                    links: true,
-                    forms: true
-                }
+               proxy: '<%= php.dist.options.hostname %>:<%= php.dist.options.port %>',
+               watchTask: true,
+               notify: true,
+               open: true,
+               logLevel: 'silent',
+               ghostMode: {
+                  clicks: true,
+                  scroll: true,
+                  links: true,
+                  forms: true
+               }
             }
-        }
-    },
-    watch: {
-        files: '*',
-    }
-});
-
-    // load plugins
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-php');
-    grunt.loadNpmTasks('grunt-browser-sync'); 
- 
-grunt.registerTask('default', [
-    'php:dist',         // Start PHP Server 
-    'browserSync:dist', // Using the php instance as a proxy 
-    'watch'             // Any other watch tasks you want to run 
-]);
-
+         }
+      },
+   });
+   
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   grunt.loadNpmTasks('grunt-php');
+   grunt.loadNpmTasks('grunt-browser-sync'); 
+   
+   grunt.registerTask('default', [
+      'php',         
+      'browserSync', 
+   ]);
 }
