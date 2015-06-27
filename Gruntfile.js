60afad0684409d520d6b32080f95515648539cb9
Stefan Kruijt <stefankruijt1991@gmail.com>
2015-06-27 13:41:54 +0200

    Gruntfile cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,49 +5,17 @@ module.exports = function(grunt) {
             options: {
                hostname: '127.0.0.1',
                port: 9000,
-               base: '.', // Project root 
+               base: '.',
                keepalive: true,
                open: false
             }
          }
-      },
-      browserSync: {
-         dist: {
-            bsFiles: {
-               src: [
-                  '*'
-               ]
-            },
-            options: {
-               proxy: '<%= php.dist.options.hostname %>:<%= php.dist.options.port %>',
-               watchTask: true,
-               notify: true,
-               open: true,
-               logLevel: 'silent',
-               ghostMode: {
-                  clicks: true,
-                  scroll: true,
-                  links: true,
-                  forms: true
-               }
-            }
-         }
-      },
-      watch: {
-         livereload: {
-            options: {
-                  livereload: 35729 
-               },
-            files: ['*']
-         }
       }
    });
    
-   grunt.loadNpmTasks('grunt-contrib-watch');
    grunt.loadNpmTasks('grunt-php');
 
    grunt.registerTask('default', [
-      'php:dist',    
-      'watch',
+      'php:dist'
    ]);
 }
